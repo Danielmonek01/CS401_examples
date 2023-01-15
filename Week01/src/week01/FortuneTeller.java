475ae865eafebc59a15c15d89161032c25205410
Pat Healy <pwh5@pitt.edu>
2023-01-14 20:05:36 -0500

    Week01 examples added

---
+++ Week01/src/week01/FortuneTeller.java
@@ -0,0 +1,40 @@
+/**
+ * 
+ */
+package week01;
+import javax.swing.JOptionPane;
+
+/**
+ * In this example, the program takes in input from the user
+ * and uses that input to tell their future.
+ * @author Pat Healy
+ *  
+ */
+public class FortuneTeller 
+{
+
+	public static void main(String[] args) 
+	{
+		// TODO Instantiate a magic number (Integer)
+		
+		// TODO Instantiate a magic divisor (Floating Point Number)
+		
+		// TODO Use a JOptionPane to welcome the user
+		// 			and ask them their name and age
+		
+		// TODO Calculate a number of days using this formula:
+		// 			Calculate integer x:
+		//				divide their age by the magic number
+		//				add this ^ to the age modulo the magic number
+		// 			Calculate integer y:
+		//				take the length of their name
+		//				divide it by the magic divisor
+		//				Round this quotient to the nearest whole number
+		
+		// Print this message to the user
+		//System.out.println("Thank you, " + name);
+		//System.out.println("The shadows from above said that you will be given an important oportunity in the future, in exactly " + days + " days from now. \nDo not miss it!");
+
+	}
+
+}
---
+++ Week01/src/week01/Variables.java
@@ -0,0 +1,35 @@
+/**
+ * 
+ */
+package week01;
+import javax.swing.JOptionPane;
+/**
+ * @author Pat Healy
+ *
+ */
+public class Variables 
+{
+
+	public static void main(String[] args) 
+	{
+		//8. Is this a legal declaration/initialization? Why?
+		// char myFavoriteCharacter = "a";
+
+		//9. How would you make a string of one character, 
+		// 		taken from the start of the string defined below?
+		// String city = "Pittsburgh";
+
+		//10. How would you calculate the length of the following string:
+		// "Java Program"
+		
+		// Swing IO Example
+		//String input = JOptionPane.showInputDialog("Write your name");
+		//JOptionPane.showMessageDialog(null,"Hi " + input);
+		
+		// 11. Given the following code, 
+		// 		how would you convert the value of myString to an integer?
+		//String myString = "745";
+		//int myInteger = ?;
+	}
+
+}
